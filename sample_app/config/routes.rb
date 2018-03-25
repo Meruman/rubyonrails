e2b0cf06546c6722c91cb45a21e9d5a325859f19
Meruman <36783697+Meruman@users.noreply.github.com>
2018-03-25 17:28:44 -0600

    sass install

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'pages/index'
 
-  root 'welcome#index'
+  get 'pages/contact'
+
+  get 'pages/about'
+
+ # get 'welcome/index'
+
+  root 'welcome#index' #Pagina principal
+
+  #get 'hello', to: 'pages#contact' #si en la url ponen www.mypage.com/hello se redirija a contact
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
